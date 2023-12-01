764b49fc49119716f88792859cc24c3b5bb8de54
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-12-01 16:26:53 +0800

    Setup bootstrap and svelte

--- inventory/settings/base.py
+++ inventory/settings/base.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["inventory/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+// https://github.com/baileyherbert/svelte-webpack-starter
+
+const path = require("path");
+const SveltePreprocess = require("svelte-preprocess");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const SvelteDevConfig = {
+    compilerOptions: {
+        dev: true
+    },
+    emitCss: false,
+    hotReload: true,
+    hotOptions: {
+        noPreserveState: false,
+        optimistic: true,
+    },
+    preprocess: SveltePreprocess({
+        scss: false,
+        sass: false,
+    })
+}
+
+const SvelteProdConfig = {
+    compilerOptions: {
+        dev: false
+    },
+    emitCss: false,
+    hotReload: false,
+    hotOptions: {
+        preserveLocalState: false,
+        noPreserveStateKey: "@!hmr",
+        noReload: false,
+        optimistic: false,
+        acceptAccessors: true,
+        acceptNamedExports: true,
+    },
+    preprocess: SveltePreprocess({
+        scss: false,
+        sass: false,
+    })
+}
+
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        style: "./inventory_fe/js/style.js",
+    },
+    resolve: {
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte')
+        },
+        extensions: [".mjs", ".js", ".ts", ".svelte"],
+        mainFields: ["svelte", "browser", "module", "main"],
+        conditionNames: ["svelte", "browser"]
+    },
+    output: {
+        path: path.resolve("./static/"),
+        filename: "[name].js",
+        chunkFilename: "chunk.[id].js",
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: "svelte-loader",
+                    options: process.env.NODE_ENV === "production" ? SvelteProdConfig : SvelteDevConfig
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /node_modules\/svelte\/.*\.mjs$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+        ]
+    }
+}

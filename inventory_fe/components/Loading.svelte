5f1600576d07475e5d9e19647f5f3d851b2ef5fe
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-12-03 12:57:39 +0800

    Add loading state

---
+++ inventory_fe/components/Loading.svelte
@@ -0,0 +1,30 @@
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Supplier</th>
+            <th scope="col">Availability</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each Array(2) as _}
+            <tr>
+                <td>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-4"></span>
+                    </p>
+                </td>
+                <td>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-6"></span>
+                    </p>
+                </td>
+                <td>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-4"></span>
+                    </p>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
--- inventory_fe/pages/inventoryList/App.svelte
+++ inventory_fe/pages/inventoryList/App.svelte
@@ -2,10 +2,13 @@
     import { onMount } from "svelte";
 
     import Empty from "../../components/Empty.svelte";
+    import Loading from "../../components/Loading.svelte";
     import Search from "../../components/Search.svelte";
     import Table from "../../components/Table.svelte";
 
     import { inventories, filteredInventories, searchKeyword } from "./store";
+
+    let loading = false;
 
     // If there's `name` search param, set it to `searchKeyword` store.
     // This would allow user to filter the inventories directly if the user
@@ -14,12 +17,15 @@
     searchKeyword.set(searchParams.get("name") || "");
 
     onMount(async () => {
+        loading = true;
         fetch("/api/inventory")
             .then((response) => response.json())
-            .then((data) => inventories.set(data))
+            .then((data) => {
+                inventories.set(data);
+                loading = false;
+            })
             .catch((error) => {
-                console.log(error);
-                return [];
+                loading = false;
             });
     });
 </script>
@@ -30,8 +36,12 @@
             <div class="card-body">
                 <Search {searchKeyword} />
                 <br />
-                {#if $filteredInventories.length}
+                {#if !loading && !$filteredInventories.length}
+                    <Empty />
+                {:else if !loading && $filteredInventories.length}
                     <Table inventories={$filteredInventories} />
+                {:else if loading && !$filteredInventories.length}
+                    <Loading />
                 {:else}
                     <Empty />
                 {/if}
